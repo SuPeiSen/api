f432c4b240bd9f66ec0fe4a9ebaa5694dfd228bf
SuPeiSen <570779284@qq.com>
2018-11-27 09:26:05 +0800

    2018.11.27\

--- application/api/controller/v1/Record.php
+++ application/api/controller/v1/Record.php
@@ -21,7 +21,7 @@ class Record extends BaseController
     {
         # 拼接产品ip，使用redis的hash自增记录每个产品点击数
         $ProductsID = 'ProductID'.Request::post('id');
-        $result = Redis::hincrby('OnClickNum',$ProductsID);
+        $result = Redis::hincrby('OnClickNum',$ProductsID,1);
         if($result) return 'success';
         return 'false';
     }
